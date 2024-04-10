a563937e0d47ba527a3ad35cae7f7147d0fe18ea
darthlilac <dessalr@live.com>
2024-04-10 15:48:10 -0300

    Módulo 34

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -35,6 +35,10 @@ import jakarta.validation.constraints.Size;
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -74,6 +78,14 @@ import jakarta.validation.constraints.Size;
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 	
